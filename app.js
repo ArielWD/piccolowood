bb1b8ed13f48414e0a5889b5d8414412f5cf299e
ArielWB <andreshernandez94.ah@gmail.com>
2023-06-16 20:42:31 -0400

    add page about and responsive

---
+++ app.js
@@ -0,0 +1,20 @@
+if (document.querySelector('#mapa')) {
+  const lat = -37.84171647593663
+  const lng = 144.94617263048812
+  const zoom = 17
+
+  // eslint-disable-next-line no-undef
+  const map = L.map('mapa').setView([lat, lng], zoom)
+
+  // eslint-disable-next-line no-undef
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map)
+
+  // eslint-disable-next-line no-undef
+  L.marker([lat, lng]).addTo(map)
+    .bindPopup(`
+        <h2 class="mapa__heading">Piccolo Wood</h2>
+    `)
+    .openPopup()
+}
