6a70cdd42540dd172b8cea40ae6294255056c795
ArielWB <andreshernandez94.ah@gmail.com>
2023-06-22 21:52:04 -0400

    add modal portfolio

--- app.js
+++ app.js
@@ -48,10 +48,7 @@ if (portafolio) {
       project.dataset.idProyecto = valor.id
       project.innerHTML += `
         <h3>${valor.nombre}</h3>
-        <picture>
-          <source srcset="${valor.img_titulo2}.webp" type="image/webp">
-          <img loading="lazy" src="${valor.img_titulo}.jpg" alt="${valor.nombre}">
-        </picture>
+        <img loading="lazy" src="${valor.imgs[0]}.webp" alt="${valor.nombre}">
       `
       project.addEventListener('click', getProject)
       portafolio.appendChild(project)
@@ -59,22 +56,93 @@ if (portafolio) {
   }
 
   async function getProject (e) {
-    const isImg = e.target.src
     try {
       const respuesta = await fetch('/projects.json')
       const resultado = await respuesta.json()
-      if (!isImg) {
-        proyectoSelect = resultado[e.target.parentElement.dataset.idProyecto]
-      } else {
-        proyectoSelect = resultado[e.target.parentElement.parentElement.dataset.idProyecto]
-      }
+      proyectoSelect = resultado[e.target.parentElement.dataset.idProyecto]
     } catch (error) {
       console.log('error')
     }
-    // console.log(proyectoSelect.ubicacion)
+    createModal(proyectoSelect)
+  }
+}
 
-    if (proyectoSelect.ubicacion !== '') {
-      console.log('si es diferente a vacio si escribo')
+function createModal (project) {
+  const modal = document.createElement('div')
+  modal.classList.add('modal')
+  const descripcion = project.descripcion.replace(/;/gi, '</br>')
+  console.log(project.imgs)
+  const images = Object.values(project.imgs)
+  console.log(images)
+  modal.innerHTML = `      
+    <span class="modal__close">&times;</span>
+    <div class="modal__content">
+      <div class="modal__info">
+        <h3 class="modal__titulo">${project.nombre}</h3>
+        ${project.ubicacion
+          ? `<div class="modal__ubicacion">
+            <h4>Ubicacion</h4>
+            <p>${project.ubicacion}</p>
+          </div>`
+        : ''}
+        <div class="modal__fecha">
+          <h4>Fecha</h4>
+          <p>${project.fecha}</p>
+        </div>
+        <div class="modal__tipo">
+          <h4>Tipo de Proyecto</h4>
+          <p>${project.tipo}</p>
+        </div>
+      </div>
+          <div class="modal__imgs slide">
+          <a class="slide__prev" >❮</a>
+          <a class="slide__next" >❯</a>
+          </div>
+      <div class="modal__descripcion">
+         <p>${descripcion}</p>
+      </div>
+    </div>
+  `
+  const boxImg = modal.querySelector('.modal__imgs')
+  console.log(boxImg)
+  for (const valor of images) {
+    const slide = document.createElement('div')
+    slide.classList.add('slide__div')
+    slide.classList.add('slide__fade')
+    const img = document.createElement('IMG')
+    img.src = valor + '.webp'
+    img.alt = project.nombre
+    img.classList.add('modal__img')
+    slide.appendChild(img)
+    boxImg.appendChild(slide)
+  }
+  window.document.body.appendChild(modal)
+  window.document.body.style.overflowY = 'hidden'
+
+  const closeModal = document.querySelector('.modal__close')
+  closeModal.addEventListener('click', () => {
+    modal.remove()
+    window.document.body.style.overflowY = 'auto'
+  })
+
+  const left = modal.querySelector('.slide__prev')
+  const right = modal.querySelector('.slide__next')
+  let slideIndex = 1
+  showSlides(slideIndex)
+  left.addEventListener('click', () => {
+    showSlides(slideIndex -= 1)
+  })
+  right.addEventListener('click', () => {
+    showSlides(slideIndex += 1)
+  })
+  function showSlides (n) {
+    let i
+    const slides = modal.getElementsByClassName('slide__div')
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = 'none'
     }
+    slides[slideIndex - 1].style.display = 'block'
   }
 }
